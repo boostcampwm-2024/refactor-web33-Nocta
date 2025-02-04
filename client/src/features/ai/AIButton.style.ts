3145c51ba0e97f9dbeef57bfd347128db1839fae
pipisebastian <yrt7998@pusan.ac.kr>
2025-02-04 12:37:32 +0900

    feat: ai Button 추가
    #20

---
+++ client/src/features/ai/AIButton.style.ts
@@ -0,0 +1,21 @@
+import { css, cx } from "@styled-system/css";
+import { glassContainer } from "@styled-system/recipes";
+
+export const glassEffect = cx(glassContainer({ border: "md" }));
+
+export const floatingButtonContainer = css({
+  zIndex: "50",
+  position: "fixed",
+  right: "24px",
+  bottom: "24px",
+});
+
+export const buttonContainer = cx(
+  glassContainer({ border: "md" }),
+  css({
+    padding: "12px",
+    paddingX: "20px",
+    backgroundColor: "white/40",
+    cursor: "pointer",
+  }),
+);
