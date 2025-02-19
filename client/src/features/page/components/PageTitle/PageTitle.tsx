39df4604743ca68abf1b1ecd4b5a2f732aab9eff
pipisebastian <yrt7998@pusan.ac.kr>
2025-02-19 14:28:25 +0900

    refactor: 제목 input를 편집기 외부에서 입력하도록 수정
    #36

--- client/src/features/page/components/PageTitle/PageTitle.tsx
+++ client/src/features/page/components/PageTitle/PageTitle.tsx
@@ -1,4 +1,5 @@
 import { PageIconType } from "@noctaCrdt/types/Interfaces";
+import { useEffect, useRef, useState } from "react";
 import { iconComponents } from "@src/constants/PageIconButton.config";
 import { pageTitleContainer, pageTitle } from "./PageTitle.style";
 
@@ -6,16 +7,79 @@ interface PageTitleProps {
   testKey: string;
   title: string;
   icon: PageIconType;
+  onTitleChange: (title: string, syncWithServer: boolean) => void;
 }
 
-export const PageTitle = ({ testKey, title, icon }: PageTitleProps) => {
+export const PageTitle = ({ testKey, title, icon, onTitleChange }: PageTitleProps) => {
   const { icon: IconComponent, color } = iconComponents[icon];
+  const [displayTitle, setDisplayTitle] = useState(title);
+  const [isTitleChange, setIsTitleChange] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTitle = e.target.value;
+    setDisplayTitle(newTitle); // 로컬 상태 업데이트
+    onTitleChange(newTitle, false); // 낙관적 업데이트
+  };
+
+  const handleDoubleClick = () => {
+    setIsTitleChange(true);
+  };
+
+  const handleBlur = () => {
+    setIsTitleChange(false);
+    onTitleChange(displayTitle, true);
+  };
+
+  const handleInput = (e: React.KeyboardEvent) => {
+    switch (e.key) {
+      case "Enter":
+      case "Escape": {
+        setIsTitleChange(false);
+        onTitleChange(displayTitle, true);
+        break;
+      }
+    }
+  };
+  useEffect(() => {
+    if (title === "새로운 페이지" || title === "") {
+      setDisplayTitle("");
+    } else {
+      setDisplayTitle(title);
+    }
+  }, [title]);
+
+  useEffect(() => {
+    if (isTitleChange && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isTitleChange]);
+
   return (
-    <div className={pageTitleContainer}>
+    <div className={pageTitleContainer} onClick={handleBlur}>
       <IconComponent color={color} size="24px" style={{ flexShrink: 0 }} />
-      <p data-testid={`pageTitle-${testKey}`} className={pageTitle}>
-        {title || "Title"}
-      </p>
+      {isTitleChange ? (
+        <input
+          ref={inputRef}
+          data-testid={`editorTitle-${testKey}`}
+          type="text"
+          placeholder="제목을 입력하세요..."
+          onChange={handleTitleChange}
+          onKeyDown={handleInput}
+          onBlur={handleBlur}
+          value={displayTitle}
+          className={pageTitle}
+        />
+      ) : (
+        <p
+          data-testid={`pageTitle-${testKey}`}
+          className={pageTitle}
+          onDoubleClick={handleDoubleClick}
+        >
+          {displayTitle || "새로운 페이지"}
+        </p>
+      )}
     </div>
   );
 };
